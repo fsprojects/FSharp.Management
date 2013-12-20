3363efe782b4deeb2af4e49e8fe7ad01af085270
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-12-20 14:19:20 +0100

    Remove "watchForChanges" since it's broken

--- src/FSharp.Management/TypeProviders.Helper.fs
+++ src/FSharp.Management/TypeProviders.Helper.fs
@@ -93,16 +93,6 @@ let seqType ty = typedefof<seq<_>>.MakeGenericType[| ty |]
 let listType ty = typedefof<list<_>>.MakeGenericType[| ty |]
 let optionType ty = typedefof<option<_>>.MakeGenericType[| ty |]
 
-/// Implements invalidation of schema when the file changes
-let watchForChanges (ownerType:TypeProviderForNamespaces) (fileName:string) = 
-    if fileName.StartsWith("http", System.StringComparison.InvariantCultureIgnoreCase) then () else
-
-    let path = Path.GetDirectoryName(fileName)
-    let name = Path.GetFileName(fileName)
-    let watcher = new FileSystemWatcher(Filter = name, Path = path)
-    watcher.Changed.Add(fun _ -> ownerType.Invalidate())
-    watcher.EnableRaisingEvents <- true
-
 // Get the assembly and namespace used to house the provided types
 let thisAssembly = System.Reflection.Assembly.GetExecutingAssembly()
 let rootNamespace = "FSharp.Management"
