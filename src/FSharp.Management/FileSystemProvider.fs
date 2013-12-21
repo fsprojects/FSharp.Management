f839c9dde4c585688948d89121decd843942e0a6
Reed Copsey, Jr. <reedc@ctech.com>
2013-12-20 17:13:27 -0800

    Remove name mangling from FileSystem typeprovider
    This addresses issue #11, and provides the API desired by @forki, namely
    FS._fszmq.Path and FS.fszmq.Path (perfectly matching the file system)

--- src/FSharp.Management/FileSystemProvider.fs
+++ src/FSharp.Management/FileSystemProvider.fs
@@ -6,6 +6,24 @@ open FSharp.Management.Helper
 open System
 open System.IO
 
+let nestedTypeWithoutNiceName<'T> typeSet typeName = 
+    let uniqueName originalName (set:System.Collections.Generic.HashSet<_>) =
+        let mutable name = originalName
+        while set.Contains name do 
+          let mutable lastLetterPos = String.length name - 1
+          while Char.IsDigit name.[lastLetterPos] && lastLetterPos > 0 do
+            lastLetterPos <- lastLetterPos - 1
+          if lastLetterPos = name.Length - 1 then
+            name <- name + "2"
+          elif lastLetterPos = 0 then
+            name <- (UInt64.Parse name + 1UL).ToString()
+          else
+            let number = name.Substring(lastLetterPos + 1)
+            name <- name.Substring(0, lastLetterPos + 1) + (UInt64.Parse number + 1UL).ToString()
+        set.Add name |> ignore
+        name
+    ProvidedTypeDefinition(uniqueName typeName typeSet, Some typeof<'T>)
+
 let createFileProperties (dir:DirectoryInfo,dirNodeType:ProvidedTypeDefinition,relative) =
     try
         for file in dir.EnumerateFiles() do
@@ -65,7 +83,7 @@ let rec annotateDirectoryNode (ownerType: ProvidedTypeDefinition) (dir: Director
     ownerType 
 
 and createDirectoryNode typeSet (dir: DirectoryInfo) propertyName =
-    annotateDirectoryNode (nestedType<obj> typeSet propertyName) dir propertyName
+    annotateDirectoryNode (nestedTypeWithoutNiceName<obj> typeSet propertyName) dir propertyName
 
 let watch dir ctx =
     let lastChanged = ref None
