c250fe7544de98ae6a1e91958349f09b9a64fdd3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-12-24 10:31:56 +0100

    XMas edition

--- src/FSharp.Management.PowerShell/AssemblyInfo.fs
+++ src/FSharp.Management.PowerShell/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Management.PowerShell")>]
 [<assembly: AssemblyProductAttribute("FSharp.Management.PowerShell")>]
 [<assembly: AssemblyDescriptionAttribute("Various type providers for management of the machine.")>]
-[<assembly: AssemblyVersionAttribute("0.0.12")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.12")>]
+[<assembly: AssemblyVersionAttribute("0.0.13")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.13")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.12"
+    let [<Literal>] Version = "0.0.13"
