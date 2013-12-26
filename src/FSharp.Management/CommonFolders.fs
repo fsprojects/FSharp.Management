96066a0a2135b60c08c80e63586eea52abe97aec
Reed Copsey, Jr <reedc@ctech.com>
2013-12-26 13:50:56 -0800

    Added CommonFolders
    Added CommonFolders to help with Issue #31

---
+++ src/FSharp.Management/CommonFolders.fs
@@ -0,0 +1,88 @@
+﻿namespace FSharp.Management
+
+open System
+open System.IO
+open System.Reflection
+
+[<AutoOpen>]
+/// Common identifiers for specific folders on the system
+module PathIdentifiers =
+    /// Folders specific to the current User
+    type UserPath =
+        | RoamingApplicationData = 26
+        | LocalApplicationData = 28
+        | Desktop = 16
+        | Documents = 5
+        | Pictures = 39
+        | Videos = 14
+        | Favorites = 6
+        | Startup = 7
+
+    // Folders shared across all users of the system
+    type SharedPath = 
+        | ApplicationData = 35
+        | Documents = 46
+        | Desktop = 25
+        | Music = 53
+        | Pictures = 54
+        | Video = 55
+
+    /// Common system folders
+    type SystemPath = 
+        | Windows = 36
+        | SystemX86 = 41
+        | System = 37
+        | Temp = -1
+
+    /// Folders related to the currently executing application
+    type ApplicationPath =
+        | EntryPointLocation = 0
+        | EntryPointShadowCopiedLocation = 1
+        | FSharpManagementLocation = 2
+        | FSharpManagementShadowCopiedLocation = 3
+
+[<RequireQualifiedAccess>]
+/// Module containing functions for working with common folder paths
+module CommonFolders =
+    let private getSpecialDirectory (pathID : int) =
+        Environment.GetFolderPath(enum<Environment.SpecialFolder>(pathID))
+
+    /// Get a folder from the set of current-user specific folders
+    let GetUser (path : UserPath) =
+        getSpecialDirectory <| int path
+
+    /// Get a folder related to the folders shared between users of the system
+    let GetSharedUser (path : SharedPath) =
+        getSpecialDirectory <| int path
+
+    /// Get a system folder
+    let GetSystem (path : SystemPath) =
+        match path with
+        | SystemPath.Temp -> System.IO.Path.GetTempPath()
+        | _ -> getSpecialDirectory <| int path
+
+    /// Get a folder specific to the currently executing application
+    let GetApplication (appPath : ApplicationPath) =
+        let (codeBasePath, path) = 
+            match appPath with
+            | ApplicationPath.EntryPointLocation -> 
+                try
+                    (true, Assembly.GetEntryAssembly().CodeBase)
+                with
+                | exn -> failwith "Unable to load entry assembly location"
+            | ApplicationPath.EntryPointShadowCopiedLocation -> 
+                try
+                    (false, Assembly.GetEntryAssembly().Location)
+                with
+                | exn -> failwith "Unable to load entry assembly location"
+            | ApplicationPath.FSharpManagementLocation -> (true, Assembly.GetExecutingAssembly().CodeBase)
+            | ApplicationPath.FSharpManagementShadowCopiedLocation -> (false, Assembly.GetExecutingAssembly().Location)
+            | _ -> failwith "Unknown path specified"
+
+        match codeBasePath with
+        | true ->
+            let uri = UriBuilder(path)
+            let unesc = Uri.UnescapeDataString uri.Path
+            Path.GetDirectoryName unesc
+        | false ->
+            Path.GetDirectoryName(path)
---
+++ tests/FSharp.Management.Tests/CommonFolders.Tests.fs
@@ -0,0 +1,66 @@
+﻿module FSharp.Management.Tests.CommonFoldersTests
+
+open FSharp.Management
+open NUnit.Framework
+open FsUnit
+
+// User Directory tests
+[<Test>]
+let ``User Roaming AppData path should match Environment.GetFolderPath``() = 
+    let env = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData)
+    CommonFolders.GetUser UserPath.RoamingApplicationData |> should equal env
+
+[<Test>]
+let ``User Local AppData path should match Environment.GetFolderPath``() = 
+    let env = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData)
+    CommonFolders.GetUser UserPath.LocalApplicationData |> should equal env
+
+[<Test>]
+let ``User desktop folder should match Environment.GetFolderPath``() = 
+    let env = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Desktop)
+    CommonFolders.GetUser UserPath.Desktop |> should equal env
+
+// Shared User  Tests
+[<Test>]
+let ``Shared AppData path should match Environment.GetFolderPath``() = 
+    let env = System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonApplicationData)
+    CommonFolders.GetSharedUser SharedPath.ApplicationData |> should equal env
+
+[<Test>]
+let ``Shared music folder should match Environment.GetFolderPath``() = 
+    let env = System.Environment.GetFolderPath(System.Environment.SpecialFolder.CommonMusic)
+    CommonFolders.GetSharedUser SharedPath.Music |> should equal env
+
+// System tests
+[<Test>]
+let ``System temp folder should match Path class``() =
+    let pathFolder = System.IO.Path.GetTempPath()
+    CommonFolders.GetSystem SystemPath.Temp |> should equal pathFolder
+
+[<Test>]
+let ``Windows system folder should contain kernel32.dll``() =
+    let sysFolder = CommonFolders.GetSystem SystemPath.System
+    let file = System.IO.Path.Combine(sysFolder, "kernel32.dll")
+    System.IO.File.Exists file |> should equal true
+
+// Application  Tests
+[<Test>]
+let ``FSharpManangement  path should contain tests\\FSharp.Manangement.Tests\\bin``() =     
+    let path = CommonFolders.GetApplication ApplicationPath.FSharpManagementLocation
+    let tempFolder = System.IO.Path.GetTempPath()
+    path |> should contain "tests\\FSharp.Management.Tests\\bin"
+
+[<Test>]
+let ``FSharpManangement shadow copied  path should contain FSharp.Manangement.Tests and Temp Folder``() =     
+    let path = CommonFolders.GetApplication ApplicationPath.FSharpManagementShadowCopiedLocation
+    let tempFolder = System.IO.Path.GetTempPath()
+    path |> should contain "FSharp.Management.Tests"
+    path |> should contain tempFolder
+
+// Unfortunatley, most test runners fail to load the entry point assembly properly.  This may be VS, or nunit, etc, but requires
+// special care to test properly
+//[<Test>]
+//let ``Entry location should contain nunit.exe``() =
+//    let path = CommonFolders.GetApplication ApplicationPath.EntryPointLocation
+//    let file = System.IO.Path.Combine(path, "nunit.ee")
+//    System.IO.File.Exists file |> should equal true
