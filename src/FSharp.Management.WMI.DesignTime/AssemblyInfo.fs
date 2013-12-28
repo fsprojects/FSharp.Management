a53b6c20cc2863cb4da5f2223f2aa8e177607376
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2013-12-28 10:07:24 +0100

    0.1.0 alpha release

--- src/FSharp.Management.WMI.DesignTime/AssemblyInfo.fs
+++ src/FSharp.Management.WMI.DesignTime/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Management.WMI.DesignTime")>]
 [<assembly: AssemblyProductAttribute("FSharp.Management.WMI.DesignTime")>]
 [<assembly: AssemblyDescriptionAttribute("Various type providers for management of the machine.")>]
-[<assembly: AssemblyVersionAttribute("0.0.13")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.13")>]
+[<assembly: AssemblyVersionAttribute("0.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.13"
+    let [<Literal>] Version = "0.1.0"
