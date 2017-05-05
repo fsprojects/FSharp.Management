6df75ac9406b58bc83dc19fcb0af74d272cc04c2
Sergey Tihon <sergey.tihon@gmail.com>
2017-05-05 21:01:51 +0300

    Released as v0.4.4

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.4.4 - 05.05.2017
+* Added support for custom runspaces (PS TP) - https://github.com/fsprojects/FSharp.Management/pull/80
+
 #### 0.4.3 - 07.11.2016
 * Updated Type Provider SDK
 
@@ -71,17 +74,17 @@
 * Unified FileSystem type provider and relative path type provider
 * Made the FileSystem type provider robust against access problems
 
-#### 0.0.4-alpha - 19.12.2013 
+#### 0.0.4-alpha - 19.12.2013
 * Relative path type provider allows to go up
 * Using latest provided types API from Sample pack
 
-#### 0.0.3-alpha - 19.12.2013 
+#### 0.0.3-alpha - 19.12.2013
 * First version of the relative path type provider
 
-#### 0.0.2-alpha - 17.12.2013 
+#### 0.0.2-alpha - 17.12.2013
 * Include FSharp.Management.PowerShell.ExternalRuntime
 
-#### 0.0.1-alpha - 17.12.2013 
+#### 0.0.1-alpha - 17.12.2013
 * Initial release of FileSystemProvider
 * Initial release of RegistryProvider
 * Initial release of PowershellProvider
--- src/FSharp.Management.PowerShell.ExternalRuntime/AssemblyInfo.fs
+++ src/FSharp.Management.PowerShell.ExternalRuntime/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Management.PowerShell.ExternalRuntime")>]
 [<assembly: AssemblyProductAttribute("FSharp.Management.PowerShell.ExternalRuntime")>]
 [<assembly: AssemblyDescriptionAttribute("Various type providers for management of the machine.")>]
-[<assembly: AssemblyVersionAttribute("0.4.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.3")>]
+[<assembly: AssemblyVersionAttribute("0.4.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.4.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Management.PowerShell.ExternalRuntime"
     let [<Literal>] AssemblyProduct = "FSharp.Management.PowerShell.ExternalRuntime"
     let [<Literal>] AssemblyDescription = "Various type providers for management of the machine."
-    let [<Literal>] AssemblyVersion = "0.4.3"
-    let [<Literal>] AssemblyFileVersion = "0.4.3"
+    let [<Literal>] AssemblyVersion = "0.4.4"
+    let [<Literal>] AssemblyFileVersion = "0.4.4"
